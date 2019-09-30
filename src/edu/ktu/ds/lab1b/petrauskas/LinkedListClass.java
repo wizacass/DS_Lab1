b588031e225b2790bc85b1c4015990bc35afe5f5
Wizacass <visak.pet0@gmail.com>
2019-09-30 18:13:09 +0300

    Rename Linked List testing class

--- src/edu/ktu/ds/lab1b/petrauskas/LinkedListClass.java
+++
@@ -1,5 +0,0 @@
-package edu.ktu.ds.lab1b.petrauskas;
-
-public class LinkedListClass
-{
-}
---
+++ src/edu/ktu/ds/lab1b/petrauskas/LinkedListTest.java
@@ -0,0 +1,17 @@
+package edu.ktu.ds.lab1b.petrauskas;
+
+import java.util.Locale;
+
+public class LinkedListTest
+{
+    private void run()
+    {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String... args)
+    {
+        Locale.setDefault(new Locale("LT"));
+        new LinkedListTest().run();
+    }
+}
